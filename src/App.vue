b24938463c2dab328ef928b5aa443ecd133a81ac
huhao <jackeysun1818@gmail.com>
2017-10-26 13:52:34 +0800

    添加工程入口页面及vue文件。

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+  // normalize.css 原生样式重置
+  @import '~normalize.css/normalize.css';
+  // 全局样式
+  @import './styles/index.less';
+</style>
+
