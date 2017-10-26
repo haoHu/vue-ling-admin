a6d61c1b64d34aafb44d29cf0bd971222c7abe49
huhao <jackeysun1818@gmail.com>
2017-10-26 09:37:26 +0800

    webpack配置，开发模式服务器配置 === 1. dev-server.js 开发模式服务器实现 	* 判断服务器环境版本是否符合 	* 配置express 	* express引入webpack中间件 	* 引入http-proxy-middleware做接口代理 2. utils.js webpack配置的方法库 	* assetsPath 配置静态资源路径 	* cssLoaders css loader配置 	* styleLoaders style Loader配置 3. webpack.dev.conf.js 开发模式下的webpack配置

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,3 +4,94 @@
  */
 // 检测环境版本
 require('./check-versions')();
+
+const config = require('../config');
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
+}
+// 在node中打开系统应用的模块
+const opn = require('opn');
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const proxyMiddleware = require('http-proxy-middleware');
+const webpackConfig = require('./webpack.dev.conf');
+
+// 开发模式下的server监听端口(default 9090)
+const port = process.env.PORT || config.dev.port;
+// 是否自动打开浏览器
+const autoOpenBrowser = !!config.dev.autoOpenBrowser;
+// 定义HTTP代理到自定义的后台api服务
+const proxyTable = config.dev.proxyTable;
+
+const app = express();
+const compiler = webpack(webpackConfig);
+
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  quiet: true
+});
+
+const hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+});
+
+// 设置当html-webpack-plugin模板改变时，强制重新加载页面
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({
+      action: 'reload'
+    });
+    cb();
+  });
+});
+
+// 代理api的请求
+Object.keys(proxyTable).forEach(function (context) {
+  let options = proxyTable[context];
+  if (typeof options === 'string') {
+    options = { target: options };
+  }
+  app.use(proxyMiddleware(options.filter || context, options));
+});
+
+// 单页应用路由处理
+app.use(require('connect-history-api-fallback')());
+
+// serve webpack bundle output
+app.use(devMiddleware);
+
+// enable hot-reload and state-preserving
+// compilation error display
+app.use(hotMiddleware);
+
+// serve pure static assets
+const staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
+app.use(staticPath, express.static('./static'));
+
+var uri = 'http://localhost:' + port;
+
+var _resolve;
+var readyPromise = new Promise(resolve => {
+  _resolve = resolve;
+});
+
+console.log('> Starting dev server ...');
+devMiddleware.waitUntilValid(() => {
+  console.log('> Listening at ' + uri + '\n');
+  // when env is testing, don't need open it
+  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
+    opn(uri);
+  }
+  _resolve();
+});
+
+var server = app.listen(port);
+module.exports = {
+  ready: readyPromise,
+  close: () => {
+    server.close();
+  }
+}
+
+
